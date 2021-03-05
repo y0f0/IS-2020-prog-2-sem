5f943de4d663eef70bbc5c39e0dd9aa99a9e6a12
Nikita <31010564+y0f0@users.noreply.github.com>
2021-03-05 14:37:26 +0300

    add fixed name

--- homework1/ClosedPolygonalChain.cpp
+++ homework1/ClosedPolygonalChain.cpp
@@ -9,11 +9,11 @@ ClosedPolygonalChain::ClosedPolygonalChain(int n, Point *points)
 ClosedPolygonalChain::ClosedPolygonalChain(const ClosedPolygonalChain &other)
   : PolygonalChain(other) {}
 
-//todo inherit =
+//fixed inherit =
 
 ClosedPolygonalChain::~ClosedPolygonalChain() { }
 
-//todo perimeter from base
+//fixed perimeter from base
 double ClosedPolygonalChain::perimeter() const {
   double res = PolygonalChain::perimeter();
   res += points_[0].getDistance(points_[n_ - 1]);
